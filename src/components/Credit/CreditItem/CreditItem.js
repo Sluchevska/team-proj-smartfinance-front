2bf68c418142f5daa4ac716f7cfa8f8ec4dfc3db
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-01-13 19:58:21 +0200

    IconButton created

--- src/components/Credit/CreditItem/CreditItem.js
+++ src/components/Credit/CreditItem/CreditItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Name, Date, Type, Sum } from './CreditItem.styled';
+import { Container, Wrapper, Name, NameInfo, Date, Type, Sum } from './CreditItem.styled';
+import IconButton from '../IconButton/IconButton';
+import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 
 const CreditItem = () => (
-    <div>
-        <Name>Метро</Name>
-        <Date>13.01.22</Date>
-        <Type>Транспорт</Type>
-        <Sum>10</Sum>
-    </div>
+    <Container>
+        <Wrapper>
+            <Name>Метро</Name>
+            <NameInfo>
+                <Date>13.01.22</Date>
+                <Type>Транспорт</Type>
+            </NameInfo>
+        </Wrapper>
+        <Sum>-10</Sum>
+        {/* <IconButton onClick={onSubmit}>
+                <SearchIcon width="40" height="40" fill = "black"/>
+        </IconButton> */}
+    </Container>
     
 );
 
