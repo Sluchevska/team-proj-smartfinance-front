45df6975e5457c405a7d5925dab59cd7914b04c6
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-01-14 16:41:41 +0200

    grid added

--- src/components/Credit/CreditItem/CreditItem.js
+++ src/components/Credit/CreditItem/CreditItem.js
@@ -1,23 +1,73 @@
-import React from 'react';
-import { Container, Wrapper, Name, NameInfo, Date, Type, Sum } from './CreditItem.styled';
-// import IconButton from '../../IconButton';
+import React, {useState, useEffect} from 'react';
+import {
+    Container,
+    WrapperInfo,
+    WrapperSum,
+    Name,
+    NameInfo,
+    Date,
+    Type,
+    Sum
+} from './CreditItem.styled';
+import IconButton from '../../IconButton';
 import { ReactComponent as DeleteIcon } from '../../../icons/delete.svg';
 
-const CreditItem = () => (
-    <Container>
-        <Wrapper>
-            <Name>Метро</Name>
-            <NameInfo>
-                <Date>13.01.22</Date>
-                <Type>Транспорт</Type>
-            </NameInfo>
-        </Wrapper>
-        <Sum>-10</Sum>
-        {/* <IconButton onClick={onSubmit}>
-                <SearchIcon width="40" height="40" fill = "black"/>
-        </IconButton> */}
-    </Container>
+import { DataGrid } from '@mui/x-data-grid';
+import data from '../../../data/data.json'
+
+const columns = [
+    { field: 'id', headerName: 'ДАТА' },
+    { field: 'name', headerName: 'ОПИСАНИЕ', width:300 },
+    { field: 'body', headerName: 'КАТЕГОРИЯ', width: 300 },
+    {field: "actions",headerName: "Actions",
+                        sortable: false,
+                        width: 100,
+                        disableClickEventBubbling: true,
+                        renderCell: (params) => {
+                            return (
+                                <div style={{ cursor: "pointer" }}>
+                                    <DeleteIcon index={params.row.id} width="18" height="18" fill="black" />
+                                 </div>
+                            );
+                         }
+                      }
+]
+
+const CreditItem = () => {
+    const [tableData, setTableData] = useState([])
     
-);
+    useEffect(() => {
+        setTableData(data);
+    },[])
+    return (
+        <div style={{height:300, width:'100%'}}>
+            <DataGrid
+                rows={tableData}
+                columns={columns}
+                pageSize ={4}
+            />
+        </div>
+        // <Container>
+        //     <WrapperInfo>
+        //         <Name>Метро</Name>
+        //         <NameInfo>
+        //             <Date>13.01.22</Date>
+        //             <Type>Транспорт</Type>
+        //         </NameInfo>
+        //     </WrapperInfo>
+        //     <WrapperSum>
+        //         <Sum>-10</Sum>
+        //         <DeleteIcon width="18" height="18" fill="black" />
+        //     </WrapperSum>
+        
+        //     {/* <IconButton>
+        //         <DeleteIcon width="40" height="40" fill = "black"/>
+        // </IconButton> */}
+        // </Container>
+    
+    );
+}
 
 export default CreditItem;
+
+// onClick={onSubmit}
