7d6eaa0564fd766ca04d56d38531bffdea63165d
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-01-16 23:00:55 +0200

    ura

--- src/components/ExpensesPage/ExpensesPage.js
+++ src/components/ExpensesPage/ExpensesPage.js
@@ -29,7 +29,7 @@ function ExpensesPage() {
                 <Box>
                     <div>
                         <Calendar />
-                       s <ExpensesInputs />
+                        <ExpensesInputs />
                     </div>
                     <Wrapper>
                         <OperationsWrapper>
