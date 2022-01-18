d078bd27e3e0b691557d24b307b6435b4c1d81ca
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2022-01-18 19:42:51 +0100

    add change

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect }  from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import balanceOperations from '../../redux/balance/balance-operations';
 // import { balanceSelectors } from '../../redux/balance/balance-selectors';
 
