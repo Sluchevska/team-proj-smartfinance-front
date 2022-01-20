d1ecc4829c29ffd6faaa3dd925750d37635ffb78
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-01-20 21:36:21 +0200

    redux operations edited

---
+++ src/redux/operations/operations-reducer.js
@@ -0,0 +1,27 @@
+//with Redux Toolkit
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit'
+
+import {
+    addOperationSuccess,
+    deleteOperationSuccess,
+    fetchOperationSuccess,
+    changeFilter
+} from './Operations-action'
+
+const items = createReducer([], {
+    [fetchOperationSuccess]:(_, {payload}) => payload,
+    [addOperationSuccess]: (state, {payload}) => [payload, ...state],
+    [deleteOperationSuccess]: (state, {payload}) =>
+        state.filter(({id}) => id !== payload)
+})
+
+const filter = createReducer('', {
+   [changeFilter]:(_, {payload}) => payload, 
+})
+
+export default combineReducers({
+    items,
+    filter
+});
+
