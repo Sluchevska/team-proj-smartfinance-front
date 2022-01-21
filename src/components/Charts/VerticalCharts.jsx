dbdd48b83354e97444d821ef05df14437552b766
TarIryna <tar86irina@gmail.com>
2022-01-21 15:36:33 +0200

    redux operations changed to reports

--- src/components/Charts/VerticalCharts.jsx
+++ src/components/Charts/VerticalCharts.jsx
@@ -1,10 +1,11 @@
 import { BarChart, Bar, Cell, LabelList, XAxis, ResponsiveContainer } from 'recharts';
+import autoMergeLevel1 from 'redux-persist/es/stateReconciler/autoMergeLevel1';
+
 export default function Charts ({data}) {
     return (
         <div>
-          <ResponsiveContainer height={400}>
+          <ResponsiveContainer height={400} margin={{top: 0, right: 40, bottom: 0, left: 40}} >
             <BarChart 
-              width={600} 
               data={data} 
               barCategoryGap={20} 
               margin={{
@@ -14,7 +15,7 @@ export default function Charts ({data}) {
                 left: 20
               }}
              >
-            <XAxis dataKey="description"  label={{fill: '#52555F', fontSize: 12, fontFamily: 'Roboto' }}/>
+            <XAxis dataKey="description" axisLine={false} tickLine={false} label={{fill: '#52555F', weight: 400, fontSize: 12, fontFamily: 'Roboto' }}/> грн
             <Bar dataKey="sum" barSize={38} radius={[10, 10, 0, 0]} label={{ position: 'top', fill: '#52555F', fontSize: 12, fontFamily: 'Roboto' }}>
             {data
               .sort((a, b) => (b.sum > a.sum ? 1 : -1))
