c8cd8baac9b4825efea6327e9c9de28df0378d2c
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-01-22 16:32:41 +0200

    Update summary-operations.js

--- src/redux/summary/summary-operations.js
+++ src/redux/summary/summary-operations.js
@@ -6,13 +6,13 @@ import {
 } from './summary-action'
 
 export const fetchSummary = (operationsType) => async dispatch => {
-    console.log("fetchSummary запрос")
+    //console.log("fetchSummary запрос")
     dispatch(fetchSummaryRequest());
 
     try {
         const { data } = await axios.get(`api/operations/bymonth?&type=${operationsType}`);
         dispatch(fetchSummarySuccess(data.data.MonthlyData));
-        console.log("summary", data)
+      //  console.log("summary", data)
     } catch (error) {
             dispatch(fetchSummaryError(error.message))
         }
